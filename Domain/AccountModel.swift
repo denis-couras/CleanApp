319e25759b7b608cff2a655f61329097ea03d9d8
Denis Couras <dcouras@stefanini.com>
2021-11-22 20:56:30 -0300

    add AddAccount use case

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Denis Couras on 22/11/21.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
