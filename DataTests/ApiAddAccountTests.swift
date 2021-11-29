c4b9c39fc75aa685394bd4dd87a3fc971db06563
Denis Couras <dcouras@stefanini.com>
2021-11-29 20:34:13 -0300

    ensure RemoteAddAccount

--- DataTests/ApiAddAccountTests.swift
+++ DataTests/ApiAddAccountTests.swift
@@ -17,32 +17,38 @@ class ApiAddAccount {
     }
     
     func add(addAccountModel: AddAccountModel) {
-        httpClient.post(url: url)
+        let data = try? JSONEncoder().encode(addAccountModel)
+        httpClient.post(to: url, with: data)
     }
 }
 
 
 protocol HttpPostClient {
-    func post(url: URL)
+    func post(to url: URL, with data: Data?)
 }
 class ApiAddAccountTests: XCTestCase {
     func testAddShouldCallHttpClientWithCorrectUrl() {
         let url = URL(string: "http://any.com")!
         let httpClient = HttpClientSpy()
         let sut = ApiAddAccount(url: url, httpClient: httpClient)
-        sut.add()
+        let addAccountModel = AddAccountModel(name: "any_name", email: "any@email.com", password: "pass", passwordConfirmation: "pass")
+        sut.add(addAccountModel: addAccountModel)
         XCTAssertEqual(httpClient.url, url)
     }
     func testAddShouldCallHttpClientWithCorrectData() {
         let httpClient = HttpClientSpy()
         let sut = ApiAddAccount(url: URL(string: "http://any.com")!, httpClient: httpClient)
-        sut.add()
+        let addAccountModel = AddAccountModel(name: "any_name", email: "any@email.com", password: "pass", passwordConfirmation: "pass")
+        sut.add(addAccountModel: addAccountModel)
+        let data = try? JSONEncoder().encode(addAccountModel)
         XCTAssertEqual(httpClient.data, data)
     }
     class HttpClientSpy: HttpPostClient {
         var url: URL?
-        func post(url: URL) {
-            
+        var data: Data?
+        func post(to url: URL, with data: Data?) {
+            self.url = url
+            self.data = data
         }
     }
 }
